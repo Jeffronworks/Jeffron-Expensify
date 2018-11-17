12b6429d05b7f787667107f74ee211a2aee2e8be
Jeffronworks <37477585+Jeffronworks@users.noreply.github.com>
2018-11-17 21:18:49 +0100

    made some changes

--- src/_test_/components/Dashboard.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import toJson from "enzyme-to-json";
-import NotFound from "../../components/NotFound";
-
-test("should render dasboard page", () => {
-  const wrapper = shallow(<NotFound />);
-  expect(toJson(wrapper)).toMatchSnapshot();
-});
