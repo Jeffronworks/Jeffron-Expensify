3bf6e0166d906982ed182f9493f69d683d9a17c9
Jeffronworks <37477585+Jeffronworks@users.noreply.github.com>
2018-11-24 16:54:37 +0100

    Setup test database enviroment

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { connect } from "react-redux";
-import numeral from "numeral";
-import SelectExpenses from "../selectors/expenses";
-import SelectExpensesTotal from "../selectors/expenses-total";
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = props => {
-  const expenseWord = props.expenseCount === 1 ? "expense" : "expenses";
-  const formattedExpenseTotal = numeral(props.expenseTotal / 100).format(
-    "$0,0.00"
-  );
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  
   return (
     <div>
-      <h1>
-        Viewing {props.expenseCount} {expenseWord} totalling{" "}
-        {formattedExpenseTotal}
-      </h1>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  const visibleExpenses = SelectExpenses(state.expenses, state.filters);
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
   return {
     expenseCount: visibleExpenses.length,
-    expenseTotal: SelectExpensesTotal(visibleExpenses)
+    expensesTotal: selectExpensesTotal(visibleExpenses)
   };
 };
 
--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,8 +1,5 @@
-const getTotalExpenses = expenses => {
-  const amountArray = expenses.map(expense => expense.amount);
-  const total = amountArray.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  }, 0);
-  return total;
+export default (expenses) => {
+  return expenses
+      .map((expense) => expense.amount)
+      .reduce((sum, value) => sum + value, 0);
 };
-export default getTotalExpenses;
